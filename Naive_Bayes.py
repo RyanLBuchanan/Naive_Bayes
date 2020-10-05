5c57a7985970a80d341710cedb0960cb1bfc137a
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-05 13:01:50 -0600

    Trained in Spyder

--- Naive_Bayes.py
+++ Naive_Bayes.py
@@ -1,2 +1,28 @@
 # Naive Bayes tutorial from Machine Learning A-Z - SuperDataScience
 # Input by Ryan L Buchanan 04OCT20
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, 2:4].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+print(classifier.predict(sc.transform([[30, 87000]])))
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)),1))
+
